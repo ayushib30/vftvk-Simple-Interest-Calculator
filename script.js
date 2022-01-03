bb07ef7d00b89a98fadee1c540cc347c82a8c717
ayushib30 <63443280+ayushib30@users.noreply.github.com>
2022-01-03 16:45:20 +0530

    get result by using innerHTML

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ function compute()
     let y = (new Date()).getYear();
     let year = t+y;
     let SInt = (p*r*t)/100;
-    return <div>
-        <span id="result-pr">If you deposit {p},</span><br>
-        <span id="result-rate">at an interest rate of {r}.</span><br>
-        <span id="result-amt">You will receive an amount of {SInt},</span><br>
-        <span id="result-year">for the year {year},</span>
-    </div>
-
+    document.getElementById("result").innerHTML = "If you deposit "+ {p} + "," + `<br/>` + "at an interest rate of " + {r} + "." + `<br/>` + "You will receive an amount of " + {SInt} + "," + `<br>` + "for the year " + {year};
 }
         
