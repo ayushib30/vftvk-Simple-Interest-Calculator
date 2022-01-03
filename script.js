98f2cc26e68444c797cf2059308722d621c70e4c
ayushib30 <63443280+ayushib30@users.noreply.github.com>
2022-01-03 16:51:30 +0530

    adding logic for computing

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    let y = (new Date()).getYear();
+    let year = t+y;
+    let SInt = (p*r*t)/100;
+    document.getElementById("result").innerHTML = "If you deposit "+ {p} + "," + `<br/>` + "at an interest rate of " + {r} + "." + `<br/>` + "You will receive an amount of " + {SInt} + "," + `<br>` + "for the year " + {year};
 }
         
