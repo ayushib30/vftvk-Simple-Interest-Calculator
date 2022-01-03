75a2740d1436e48ea0d114c0415658b406a01858
ayushib30 <63443280+ayushib30@users.noreply.github.com>
2022-01-03 16:18:08 +0530

    added result div

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    let y = (new Date()).getYear();
+    let year = t+y;
+    let SInt = (p*r*t)/100;
+    return <div>
+        <span id="result-pr">If you deposit {p},</span><br>
+        <span id="result-rate">at an interest rate of {r}.</span><br>
+        <span id="result-amt">You will receive an amount of {SInt},</span><br>
+        <span id="result-year">for the year {year},</span>
+    </div>
+
 }
         
